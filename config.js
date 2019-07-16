51e3339f7132e7622f8d3177f9d7e2f44536e35f
achuthrajula <achuthrajula1999@gmail.com>
2019-07-16 16:22:42 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretkey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '365693977479144',
+        clientSecret: '7ec1ae01acfd9555a63764320771666d'
+    }
 }
